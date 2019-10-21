d1dcec2f9503fb647a79166c9beadcbaa93ed475
Rodrigo Nogueira <rdg6design@gmail.com>
2019-10-21 16:16:31 -0300

    ok

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,6 +1,7 @@
 class WhatsAppController{
     constructor(){
         console.log('WhatsAppController ok')
+        this.elementsPrototype();
         this.loadElements();
     }
 
@@ -9,5 +10,18 @@ class WhatsAppController{
         document.querySelectorAll('[id]').forEach(element=>{
             this.el[Format.getCamelCase(element.id)] = element;
         })
+    }
+    elementsPrototype(){
+        Element.prototype.hide = function(){
+            this.style.display = 'none'; 
+        }
+        Element.prototype.show = function(){
+            this.style.display = 'block'; 
+        }
+        Element.prototype.toggle = function(){
+            this.style.display = (this.style.display === 'none') ? 'block':'none'; 
+        }
+
+         
     }
 }
